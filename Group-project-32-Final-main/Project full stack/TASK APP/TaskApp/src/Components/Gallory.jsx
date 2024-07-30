f432fc79b3d02d326c1321268ddb3d0cc5ac48b7
sandieagle <jayasundarasanduni49@gmail.com>
2024-07-30 08:52:37 +0530

    upload again

---
+++ Group-project-32-Final-main/Project full stack/TASK APP/TaskApp/src/Components/Gallory.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import Gallorycont from "./Gallorycont";
+import "./style.css"; // Import CSS file
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  // JavaScript function to handle navigation
+  const handleNavigate = (url) => {
+    // You can customize this function to handle navigation as needed
+    navigate(url);
+  };
+  
+  return (
+    <div>
+    <div>
+    <div className="navbar">
+      <div className="navbar-brand">
+        <span className="app-name">UTMS</span>
+      </div>
+      <div className="navbar-tabs">
+        <button className="tab" onClick={() => handleNavigate("/")}>Home</button>
+        <button className="tab" onClick={() => handleNavigate("/about")}>About</button>
+        <button className="tab" onClick={() => handleNavigate("/gallory")}>Gallery</button>
+      </div>
+      <div className="navbar-right">
+        <button type="button" className="btn btn-primary" onClick={() => handleNavigate('/employee_login')}>
+          Employee
+        </button>
+        <button type="button" className="btn btn-success" onClick={() => handleNavigate('/adminlogin')}>
+          Admin
+        </button>
+      </div>
+    </div>
+    </div> 
+    <div className="space"></div>   
+    <Gallorycont/>
+    <div>
+      <Footer/>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
